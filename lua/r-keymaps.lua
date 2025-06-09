52a2ed45d1a14f7ed058ecd79bb37635ccbb19af
Leandro <leandro.leon@stocksnips.net>
2025-06-09 13:11:00 +0000

    fzf changes and fix for commenting R chunks

--- lua/r-keymaps.lua
+++ lua/r-keymaps.lua
@@ -1,8 +1,12 @@
+-- r-keymaps.lua  ── fixed
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'r', 'rmd', 'rmarkdown' },
-  callback = function(args)
-    vim.diagnostic.config({
+  callback = function()
+    -- just one table; no second arg
+    vim.diagnostic.config {
+      virtual_text = false,
+      severity_sort = true,
       severity = { min = vim.diagnostic.severity.WARN },
-    }, args.buf)
+    }
   end,
 })
